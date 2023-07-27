2589863d3feeb8dc89c75814df94fe128831056c
Brian <mahuibrian14@gmail.com>
2023-07-27 14:35:17 +0300

    REDUX

--- client/src/Reducers/rootReducers.js
+++ client/src/Reducers/rootReducers.js
@@ -1,9 +1,11 @@
 import { combineReducers } from '@reduxjs/toolkit';
+import EventReducer from './EventReducer';
 //import other reducers here
 
 const rootReducer = combineReducers({
    // Add your reducers here
   // Add other reducers as needed
+  events: EventReducer,
 });
 
 export default rootReducer;
