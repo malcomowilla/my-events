7cfa7515921b2c68f125be633af316b11953a262
malcomowilla <owillamalcom@gmail.com>
2023-07-29 11:32:16 +0300

    abandoned the use of seatgeek api now using faker

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,4 @@
 class EventSerializer < ActiveModel::Serializer
   attributes :id, :name, :image_url, :date, :description, :location, :organizer_id
-      has_many :perfomers
 
 end
--- app/serializers/perfomer_serializer.rb
+++
@@ -1,4 +0,0 @@
-class PerfomerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :type, :image_url, :has_upcoming_events, :event_id
-  belongs_to :event
-end
