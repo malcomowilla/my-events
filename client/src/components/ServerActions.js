fe14a619d1b0fc75fc63949ee62712a43382b1e3
Brian <mahuibrian14@gmail.com>
2023-08-01 18:44:27 +0300

    communication with server

--- client/src/components/ServerActions.js
+++ client/src/components/ServerActions.js
@@ -1,12 +1,18 @@
 //FetchEvents handles data fetch
 /*for user*/
 
-import { newUser } from "../Reducers/UserReducer";
+
 
 export const FetchEvents = () => async (dispatch) => {
     try {
-      const eventsUrl = "https://event-manager-1mtv.onrender.com/events";
-      const response = await fetch(eventsUrl);
+      const eventsUrl = "http://127.0.0.1:3000/events";
+      const response = await fetch(eventsUrl,{
+        method: "GET",
+        headers: {
+          "Accept": "application/json", // Specify that the client expects a JSON response
+          // You can add more headers here if needed
+        },
+      });
       const eventData = await response.json();
       dispatch({ type: "FETCH_EVENTS_SUCCESS", payload: eventData });
     } 
@@ -21,20 +27,20 @@ export const SIGNUP_SUCCESS = "SIGNUP_SUCCESS";
 export const SIGNUP_FAILURE = "SIGNUP_FAILURE";
 
 
-export const NewUser=()=> async (dispatch) => {
+export const NewUser=(userDetails)=> async (dispatch) => {
   try {
-    const signUpUrl="https://localhost/signup" ;
+    const signUpUrl="http://127.0.0.1:3000/signup" ;
     const response = await fetch(signUpUrl, {
       method: "POST",
       headers: {
         "Content-Type": "application/json", 
       },
-      body: JSON.stringify(newUser), // Convert the userData to JSON string and send it in the request body
+      body: JSON.stringify(userDetails), // Convert the userData to JSON string and send it in the request body
     });
     if (response.ok) {
       
       
-      dispatch({ type: "SIGNUP_SUCCESS", payload: newUser });
+      dispatch({ type: "SIGNUP_SUCCESS", payload: userDetails });
     } else {
       
       dispatch({ type: "SIGNUP_FAILURE", payload: "Error signing up." });
