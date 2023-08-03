264fc756af95f9db10a38d983a7ffec5a31408b0
certihimo <kalidahm3d@gmail.com>
2023-08-03 17:55:08 +0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :attendees,only: [:index, :show, :create]
+  resources :attendees, only: [:index, :show, :create]
   resources :tickets, only: [:index, :show, :create]
   resources :events, only: [:index, :show, :create]
 end
