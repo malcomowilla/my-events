5ce1c1f7e97b0309cdabc5d13e14195487feb3ae
certihimo <kalidahm3d@gmail.com>
2023-08-03 09:17:48 +0300

    done

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,14 @@
 class Event < ApplicationRecord
-   
-end
+          validates :title, presence: true
+  validates :description, presence: true
+  validates :location, presence: true
+  alidates :age_limit, presence: true
+  validates :capacity, numericality: { greater_than: 0 }
+  validates :date, presence: true
+  has_many :attendees
+  belongs_to :sponsor
+  has_many :speakers, dependent: :destroy
+  belongs_to :user
+  #belongs_to :speaker
 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :attendees,only: [:index, :show, :create]
+  resources :tickets
   resources :events, only: [:index, :create, :show]
 
 end
