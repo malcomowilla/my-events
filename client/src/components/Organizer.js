ea5d9fd217614796f96a0d0a5577667a1991430a
Brian <mahuibrian14@gmail.com>
2023-08-04 11:12:43 +0300

    Organizer component

---
+++ client/src/components/Organizer.js
@@ -0,0 +1,15 @@
+const Organizer=()=>{
+//sign up as an organizer,
+//login as an organizer,
+//if logged in, redirect to events CRUD
+
+return(
+    <>
+      <h2>Sign up as an organizer</h2>
+      
+      <h2>Login</h2>
+    
+    </>
+)
+}
+export default Organizer;
