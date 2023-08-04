21ef24f6857d3c1d00b32c45266d8f1ae39aa478
Brian <mahuibrian14@gmail.com>
2023-08-04 14:54:36 +0300

    Ticket,ticketCart functionality

--- client/src/Reducers/ticketActions.js
+++ client/src/Reducers/ticketActions.js
@@ -1,9 +1,16 @@
 
 export const ADD_BOOKED_TICKET = "ADD_BOOKED_TICKET";//action type to add ticket
-
+export const REMOVE_BOOKED_TICKET= "REMOVE_BOOKED_TICKET";//action type to remove booked ticket
 export const addBookedTicket = (ticket) => {
     return {
       type: ADD_BOOKED_TICKET,
       payload: ticket,
     };
 };
+export const removeBookedTicket = (ticket) => {
+  return {
+    type: REMOVE_BOOKED_TICKET,
+    payload: ticket,
+  };
+};
+
