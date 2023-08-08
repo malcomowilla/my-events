67d4803ed045bb29704b68eefce40a49076f64c1
Brian <mahuibrian14@gmail.com>
2023-08-08 12:54:57 +0300

    Setting up signup and login for organizers

--- app/controllers/organizers_controller.rb
+++ app/controllers/organizers_controller.rb
@@ -19,16 +19,53 @@ class OrganizersController < ApplicationController
     end
   end
 
+  skip_before_action :authorized, only: [:create_user]
+
+    
+
+    def signup_organizer
+        organizer = Organizer.create(organizer_params)
+       if organizer.valid?
+         
+        token = encode_token({organizer_id: organizer.id})
+                
+
+        render json: {organizer: OrganizerSerializer.new(organizer), jwt: token}, status: :created
+        else
+            puts "Failed to create organizer!"
+    puts user.errors.full_messages
+      render json: { error: 'failed to create organizer' }, status: :unprocessable_entity
+    end
+    end 
+    #login login POST   /login(
+    def login_organizer
+        organizer = Organizer.find_by(name: params[:name] )
+
+        if organizer && organizer.authenticate(params[:password])
+            session[:organizer_id] = organizer.id
+
+            token = encode_token( organizer_id: organizer.id) 
+                
+
+            render json: {user: OrganizerSerializer.new(organizer), jwt: token}, status: :accepted
+        else
+            render json: { message: 'Invalid username or password', error: 'Authentication failed' }, status: :unauthorized
+        end
+    end
+    def destroy
+      session.delete :organizer_id
+      head :no_content
+    end
+
+
   private
+  def organizer_params
+    params.permit(:name, :password, :email)
+    end
 
   def event_params
     params.require(:event).permit(:name, :image_url, :date, :description, :location)
   end
 end
-
-
-
-
-
 
 end
