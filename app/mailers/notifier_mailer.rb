1817c99f217aebbe729a093f1148c0b7ad45e957
malcomowilla <owillamalcom@gmail.com>
2023-08-08 19:10:02 +0300

    troble shooting render

--- app/mailers/notifier_mailer.rb
+++
@@ -1,7 +0,0 @@
-# class NotifierMailer < ApplicationMailer
-#     def alert_user
-#         mail(to: "owillamalcom@gmail.com", subject: "ALERT - welcome to events and ticketing" )
-
-
-#     end
-# end
