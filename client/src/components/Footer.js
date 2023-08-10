68931ef9d3ee2417b6c2254e2fcb2dbfd99635b0
Brenda <brendakajuju777@gmail.com>
2023-08-10 16:16:55 +0300

    add home and footer mods

---
+++ client/src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Footer.css"
+import { Link } from "react-router-dom"
+
+function Footer() {
+    return (
+        <div className='footer-container'>
+            <ul className='footer-list'>
+                <li><h2>ABOUT</h2></li>
+                <li><p>Contact us</p></li>
+                <li><p>About us</p></li>
+                <li><p>Careers</p></li>
+                <li><p>Gift Cards</p></li>
+            </ul>
+            <ul className='footer-list'>
+                <li><h2>HELP</h2></li>
+                <li><p>Payments</p></li>
+                <li><p>Cancellation & Returns</p></li>
+                <li><p>FAQs</p></li>
+            </ul>
+        
+        </div>
+    )
+}
+
+export { Footer } 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,6 +3,9 @@ import React from "react";
 import { useSelector } from "react-redux";
 import EventItem from "./EventItem";
 import { useNavigate } from "react-router-dom";
+import './Home.css'
+import CityIllustration from "../assets/lofi-vaporwave.gif"
+
 
 const Home=()=>{
   const navigate=useNavigate()
@@ -16,25 +19,25 @@ const Home=()=>{
   // Limit the events to 6 
   const homePageEvents = events.slice(0, 6);
     return(
-        <>
-          <h1>Event Ticketing website</h1>
-          <h2>there should be an animation here,some styling n stuff,logo of the website,about us,promos etc.
-            WATU wa Css
-          </h2>
-          <br />{" "}
+      <div className='home-component-container'>
+        <div className='home-page-img-container'>
+        <img className="home-page-background-img" src={CityIllustration} alt="Library Illustration"/>
+        </div>
+      <h1 className='homepage-headings'>Highlights</h1>
           <p>Do you have events you want to share with us?
             Sign up <button onClick={toSignUp}>here</button> as an organizer to host them with us!
           </p>
-          <button onClick={allEvents}>
-            See more events...
+          <button className="solid-secondary-btn homepage-explore-all-btn" onClick={allEvents}>
+            Explore All events
           </button>
-          <br />{" "}
           <div>
             {homePageEvents.map((event) => (
              <EventItem key={event.id} event={event} />
+             
           ))}
           </div>
-        </>
+        </div>
+        
     )
 }
 export default Home;
